02222b0a9934fc74c834733a8b96a8879ce2fff9
Alex-ABWorld <alexbu2015@hotmail.com>
2021-11-19 18:22:53 +0000

    Add DefaultTranslatedTarget

---
+++ ABCo.ABPaint/ABCo.ABPaint.Core/Rendering/Targets/DefaultTranslatedTarget.cs
@@ -0,0 +1,31 @@
+﻿using ABCo.ABPaint.Core.Representation.Patterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCo.ABPaint.Core.Rendering.Targets
+{
+    public class DefaultTranslatedTarget : IDrawTarget
+    {
+        IDrawTarget _base;
+
+        int _translateX;
+        int _translateY;
+
+        public int PixelWidth => _base.PixelWidth;
+        public int PixelHeight => _base.PixelWidth;
+
+        public DefaultTranslatedTarget(int translateX, int translateY, IDrawTarget @base) => (_translateX, _translateY, _base) = (translateX, translateY, @base);
+
+        public void FillRectangle(int x, int y, int width, int height, Pattern pattern)
+        {
+            _base.FillRectangle(x + _translateX, y + _translateY, width, height, pattern);
+        }
+
+        // TODO: Better exception objects?
+        public void Start() => throw new Exception("Cannot start a 'DefaultTranslatedTarget'");
+        public void Finish() => throw new Exception("Cannot start a 'DefaultTranslatedTarget'");
+    }
+}
---
+++ ABCo.ABPaint/tests/ABCo.ABPaint.Core.UnitTests/Rendering/Targets/DefaultTranslatedTargetTests.cs
@@ -0,0 +1,34 @@
+﻿using ABCo.ABPaint.Core.Rendering;
+using ABCo.ABPaint.Core.Rendering.Targets;
+using ABCo.ABPaint.Core.Representation.Patterns;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCo.ABPaint.Core.UnitTests.Rendering.Targets
+{
+    [TestClass]
+    public class DefaultTranslatedTargetTests
+    {
+        [TestMethod]
+        public void Start_ThrowsException() => Assert.ThrowsException<Exception>(() => new DefaultTranslatedTarget(0, 0, Substitute.For<IDrawTarget>()).Start());
+
+        [TestMethod]
+        public void Finish_ThrowsException() => Assert.ThrowsException<Exception>(() => new DefaultTranslatedTarget(0, 0, Substitute.For<IDrawTarget>()).Finish());
+
+        [TestMethod]
+        public void FillRectangle_CallsTransformed()
+        {
+            var baseTarget = Substitute.For<IDrawTarget>();
+            var target = new DefaultTranslatedTarget(15, 25, baseTarget);
+
+            target.FillRectangle(50, 70, 20, 30, Pattern.White);
+
+            baseTarget.Received().FillRectangle(65, 95, 20, 30, Pattern.White);
+        }
+    }
+}
